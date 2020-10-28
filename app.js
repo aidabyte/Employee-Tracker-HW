645340ea6fbc8514f27da9cf2846ee1b9abb03c9
aidabyte <ablinstrubyte@gmail.com>
2020-10-28 18:08:17 -0500

    writing out switch statment

--- app.js
+++ app.js
@@ -1,56 +1,77 @@
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-const tables = require("console.tables")
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+// const consoleTables = require ('console.tables')
 
-// any port can connect
-var PORT = process.env.PORT || 3000;
-// MySQL DB Connection Information 
+
 var connection = mysql.createConnection({
-  
+
 
   host: "localhost",
 
+  // Your port; if not 3306
+  PORT: 3000,
 
-  port : 3000,
-
-// change this to your own
+  // Your username
   user: "root",
 
-
+  // Your password
   password: "password",
 
 
+
   database: "employee_db"
-
-
-  });
-  connection.connect(function(err) {
+});
+connection.connect(function (err) {
     if (err) throw err;
-    // run the start function after the connection is made to prompt the user
+    console.log("connected");
     start();
-  });
+    // connection.end();
+});
 
 
-  function start() {
-    inquirer
-      .prompt({
-        name: "userAction",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-         "view all employees",
-         "view all departments", 
-         "view all roles",
-         "add an employee",
-         "remove employee",
-         "update an employee",
-         "add a role",
-         "remove a role",
-         "add a department",
-         "remove a department",
+function start() {
+  inquirer
+    .prompt({
+      name: "choice",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+       "view all employees",
+       "view all departments", 
+       "view all roles",
+       "add an employee",
+       "remove employee",
+       "update an employee",
+       "add a role",
+       "remove a role",
+       "add a department",
+       "remove a department",
+       "exit"
 
-        ]
-      })
+      ]
+    }).then(function (response) {
+      
+      switch (response.choice){
+       case "view all employees":
+         break;
+        
+        case "view all departments":
+          break;
+
+        case "view all roles":
+          break;
+
+        case "add an employee":
+          break;
+        
+        case "remove employee":
+          break;
+
+        case "update an employee":
+        break;
+
+        
+      }
     }
-     
+    )};
 
